ed48bf24ddaca93b869326769e131cda19fd4139
unknown <alexkana@csd.auth.gr>
2022-07-19 03:50:12 +0300

    Update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,17 @@
 import {Container,Nav,Navbar as NavBoot} from "react-bootstrap"
-import { Link, NavLink,useNavigate } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 import logo from '../assets/logo.png';
 
 //Navbar of the website
 
 export function Navbar() {
-  const navigate = useNavigate();
-
-  function changeLocation(placeToGo){
-    navigate(placeToGo, { replace: true });
-    window.location.reload();
-}
-
     return (
      <NavBoot className="bg-white mb-3 shadow">
         <Container>
           <Nav>
             <Nav.Item as={NavLink} to="/"><img src={logo} alt="Logo" style={{height:"1.8rem", width:"1.8rem", marginTop:"5px"}}/></Nav.Item>
             <Nav.Link as={NavLink} to="/">Home</Nav.Link>
-            <Nav.Link as={NavLink} to="/add" onClick={() => changeLocation('/add')}>Add</Nav.Link>
+            <Nav.Link as={NavLink} to="/add">Add</Nav.Link>
             <Nav.Link as={NavLink} to="/about">About</Nav.Link>
           </Nav>
         </Container>
